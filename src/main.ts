8fe4e8e15ccd9eafd0cf515a6298197fa2c3a693
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2023-11-29 15:39:42 +0600

    Update main ts

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import type { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function start() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const port = process.env.PORT || 3000;
 
-  const corsOptions = {
+  const corsOptions: CorsOptions = {
     origin: 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    maxAge: 24 * 60 * 60 * 5,
   };
 
   app.enableCors(corsOptions);
@@ -22,3 +24,4 @@ async function start() {
   );
 }
 start();
+export default start;
